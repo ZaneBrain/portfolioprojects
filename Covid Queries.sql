c4e1528a4481bc4b13e275f79f50f48dd5aa4e64
ZaneBrain <111667640+ZaneBrain@users.noreply.github.com>
2022-08-20 17:52:46 -0700

    Add files via upload

---
+++ Covid Queries.sql
@@ -0,0 +1,143 @@
+--Data exploration by Location
+--International and Northern Cyprus have NULL values for their populations
+--Location isn't just countries: World, Asia, Lower middle income, Upper middle income, Africa, High income, Europe, Low income
+--North America, European Union, South America, International
+--All of these have a NULL continent
+SELECT continent, location, MAX(population) AS Max_Pop
+FROM [covid deaths]
+GROUP BY continent, location
+ORDER BY Max_Pop desc
+
+
+--Current Probability of death if COVID has been contracted, by country
+SELECT location, date, population, total_cases, total_deaths, (total_deaths/total_cases)*100 AS percent_died
+FROM [Covid Portfolio Project]..[covid deaths]
+WHERE continent IS NOT NULL
+ORDER BY 2 desc, 1 
+
+--Total cases by population (not accounting for repeat cases)
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS percent_contracted
+FROM [covid deaths]
+WHERE continent IS NOT NULL 
+ORDER BY 2 desc, 1
+
+--Countries with Highest Infection Rate Compared to Population
+SELECT location, MAX(total_cases) AS greatest_number_infected, MAX(total_cases/population) AS percent_pop_infected
+FROM [covid deaths]
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY 3 desc
+
+--Countries with Highest Mortality (North Korea is an Outlier—6 deaths reported, but only 1 case reported)
+SELECT location, MAX(total_deaths) AS total_deaths, MAX(total_cases) AS total_cases, MAX(total_deaths)/MAX(total_cases) AS chance_of_death
+FROM [covid deaths]
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY 4 desc
+
+--North Korea sees its first case reported on 14/5/2022 in this data, with reports of 6 deaths coinciding
+--Presumably this is due to unreliable reporting
+SELECT location, date, total_cases, total_deaths
+FROM [covid deaths]
+WHERE location = 'North Korea'
+ORDER BY 4 desc, 3 desc, 2
+
+--Calculating statistics by Continent
+--Percent contracted
+SELECT location, date, population, total_cases, (total_cases/population)*100 AS percent_contracted
+FROM [covid deaths]
+WHERE continent IS  NULL 
+ORDER BY 1, 2
+
+--Continents with Highest Infection Rate Compared to Population
+SELECT location, MAX(total_cases) AS greatest_number_infected, MAX(total_cases/population) AS percent_pop_infected
+FROM [covid deaths]
+WHERE continent IS NULL and location NOT IN ('World', 'High Income', 'Upper middle income', 'Lower middle income', 'Low income', 'International')
+GROUP BY location
+ORDER BY 2 desc, 1
+
+--Finding the amount of new cases and deaths globally by day
+SELECT date, SUM(new_cases) AS case_total, SUM(new_deaths) AS death_total, SUM(new_deaths)/sum(new_cases) AS mortality_ratio
+FROM [covid deaths]
+WHERE continent IS NOT NULL
+GROUP BY date
+order by 4 desc
+
+--Finding the total global mortality
+SELECT MAX(total_cases) AS Total_Cases, MAX(total_deaths) AS Total_Deaths, MAX(total_deaths)/MAX(total_cases)*100 AS Overall_Mortality
+FROM [covid deaths]
+
+--Finding the total global mortality
+SELECT SUM(new_cases) AS Total_Cases, SUM(new_deaths) AS Total_Deaths, SUM(new_deaths)/SUM(new_cases)*100 AS Overall_Mortality
+FROM [covid deaths]
+WHERE continent IS NOT NULL 
+
+--Testing table join
+SELECT *
+FROM [covid deaths] death
+JOIN [covid vaccinations] vac
+	ON death.location = vac.location
+	and death.date = vac.date 
+
+--Total Population vs Vaccinations
+SELECT death.location, death.date, death.population, new_vaccinations, 
+	SUM(new_vaccinations) OVER (Partition by death.location ORDER BY death.location, death.date) AS running_total_vac
+FROM [covid deaths] death
+JOIN [covid vaccinations] vac
+	ON death.location = vac.location
+	and death.date = vac.date 
+WHERE death.continent IS NOT NULL 
+ORDER BY 1, 2
+
+
+--Creating a Common Table Expression of the above query
+
+With PopvsVac (Location, Date, Population, New_Vaccinations, Rolling_Total_Vac)
+AS
+(
+	SELECT death.location, death.date, death.population, new_vaccinations, 
+		SUM(new_vaccinations) OVER (Partition by death.location ORDER BY death.location, death.date) AS running_total_vac
+	FROM [covid deaths] death
+	JOIN [covid vaccinations] vac
+		ON death.location = vac.location
+		and death.date = vac.date 
+	WHERE death.continent IS NOT NULL 
+)
+SELECT *, (Rolling_Total_Vac/Population)*100
+FROM PopvsVac 
+
+--Creating a Temporary Table of the above query
+DROP TABLE IF EXISTS #PoptoVac
+CREATE TABLE #PoptoVac
+(Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_Vaccinations numeric,
+Running_Total_Vac numeric
+)
+INSERT INTO #PoptoVac
+SELECT death.location, death.date, death.population, new_vaccinations, 
+	SUM(new_vaccinations) OVER (Partition by death.location ORDER BY death.location, death.date) AS running_total_vac
+FROM [covid deaths] death
+JOIN [covid vaccinations] vac
+	ON death.location = vac.location
+	and death.date = vac.date 
+WHERE death.continent IS NOT NULL 
+
+SELECT *, (Running_Total_Vac/Population)*100 AS Percent_Vac
+FROM #PoptoVac
+
+--Creating View for later Viz
+
+CREATE VIEW PoptoVac AS
+SELECT death.location, death.date, death.population, new_vaccinations, 
+	SUM(new_vaccinations) OVER (Partition by death.location ORDER BY death.location, death.date) AS running_total_vac
+FROM [covid deaths] death
+JOIN [covid vaccinations] vac
+	ON death.location = vac.location
+	and death.date = vac.date 
+WHERE death.continent IS NOT NULL 
+
+SELECT *
+FROM PoptoVac
+
